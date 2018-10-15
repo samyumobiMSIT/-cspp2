574620b8b568797c528991662e9ddad74c882593
samyumobiMSIT <samyumobi@msitprogram.net>
2018-10-15 17:29:01 +0530

    cspp2-m1-a14

---
+++ practice/m1/degrees_to_fahrenheit.java
@@ -0,0 +1,11 @@
+//m1-a14 Program to find Quotient And Remainder
+import java.util.Scanner;
+class degrees_to_fahrenheit{
+	public static void main(String ar[]){
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter degrees");
+		float d=s.nextFloat();
+		float f= d * (9f / 5) + 32;
+		System.out.println("d: "+ d+ "f: "+ f);
+	}
+}
