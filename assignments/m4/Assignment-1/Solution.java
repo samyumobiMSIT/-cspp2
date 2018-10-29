5d75935dcc77e36fa57b4f5e95c39f53b5388de3
samyumobiMSIT <samyumobi@msitprogram.net>
2018-10-29 14:16:17 +0530

    hello
    hello

---
+++ assignments/m4/Assignment-1/Solution.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+public class Solution
+{/*
+	Fill this main function to print maximum of given array
+	*/
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int n= sc.nextInt();
+		
+
+	}
+}
