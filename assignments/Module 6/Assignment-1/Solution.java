99e1e072708a2700a55338204e80403d314980b7
samyumobiMSIT <samyumobi@msitprogram.net>
2018-12-04 17:27:19 +0530

    "m6_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ assignments/Module 6/Assignment-1/Solution.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+/**
+ * Write a java program to find the odd composite numbers
+ * between 2 and n where n is given as an input.
+ *
+ * 
+ */
+public final class Solution {
+    /**
+    * Empty constructor.
+    */
+    private Solution() {
+        //not used
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      n     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    static boolean isprime(final int n) {
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                count++;
+            }
+        }
+        if (count == 2) {
+            return true;
+        }
+        return false;
+    }
+    /**
+     * Prints the odd composite numbers between 2 and n.
+     *
+     * @param      n     n value
+     */
+    static void oddComposites(final int n) {
+        final int three = 3;
+        for (int i = three; i <= n; i++) {
+            if (i % 2 != 0 && !isprime(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+    /**
+    * main method as driver program.
+    * @param args is the parameter for this method
+    */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        oddComposites(n);
+    }
+}
